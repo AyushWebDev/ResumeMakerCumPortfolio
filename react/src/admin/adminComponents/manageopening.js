a8929fa520845564ce9412e28fae1726fdc20ffd
Harsh Shukla <harsh.307.shuklaaa@gmail.com>
2021-12-11 16:58:18 +0530

    Admin profile page `

---
+++ react/src/admin/adminComponents/manageopening.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ManageOpenening extends Component {
+    render() {
+        return (
+            <div>
+                    <p>Hello Harsh</p>
+                    <p>This Component is Manage opening</p>
+            </div>
+        );
+    }
+}
+
+export default ManageOpenening;
