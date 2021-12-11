8d41e8bc222cdaa4928d3e7dfa716ba84156584a
Harsh Shukla <52241727+harsh212@users.noreply.github.com>
2021-12-11 20:05:40 +0530

    Harsh changes (#13)
    * styling modal
    
    * Added Signout Modal in Profile page of user
    
    * Added Admin SignIn page
    
    * Signup Admin Page
    
    * Admin profile page
    `
    
    * Update
    
    * Update
    
    * update

--- react/src/admin/admin_profile.js
+++ react/src/admin/admin_profile.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { signout } from '../user/auth';
-import { getBasics,isAuthenticated,delEdu} from '../user/auth';
+import { getBasics,signout,isAuthenticated} from '../admin/auth';
 import {BrowserRouter,Switch,Route,Link} from 'react-router-dom';
 import { Layout, Menu, Breadcrumb,Row,Col } from 'antd';
 import {
@@ -13,9 +12,50 @@ import {
 } from '@ant-design/icons';
 import Text from 'antd/lib/typography/Text';
 import ManageOpenening from './adminComponents/manageopening';
+import CreateJob from './adminComponents/createjob';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 class AdminProfile extends Component {
+    constructor()
+    {
+        super();
+        this.state={
+            id: "",
+            firstname: '',
+            lastname: '',
+            org_email: '',
+            address: '',
+            org_title:'',
+            error: "",
+            collapsed:false
+        }
+    }
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+  componentDidMount(){
+        const getBasicsDetails=async (id)=>{
+            const data=await getBasics(id);
+            console.log(data);
+            if(data.error){
+                console.log(data.error);
+            }
+            else{
+                this.setState({
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    org_email: data.email,
+                    address: data.address,
+                    org_name:data.orgname,
+                    error: ""
+                },console.log(this.state));
+            }
+        }
+        const id=this.props.match.params.empid;
+        getBasicsDetails(id);
+    }
     render() {
         return (
         <div>
@@ -28,12 +68,12 @@ class AdminProfile extends Component {
           left: 0,
          }}>
           <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" style={{fontSize:'17px',marginTop:'30%'}}>
+          <Menu theme="dark"  mode="inline" style={{fontSize:'17px',marginTop:'30%'}}>
             <Menu.Item key="profile" icon={<PieChartOutlined />}>
-            Manage Openings
+            <Link to={`/admin-profile/${isAuthenticated().emp._id}/manage/${isAuthenticated().emp._id}`}>Manage Openings</Link>
             </Menu.Item>
             <Menu.Item key="2" icon={<UsergroupAddOutlined />}>
-            Create Job 
+            <Link to={`/admin-profile/${isAuthenticated().emp._id}/create-job/${isAuthenticated().emp._id}`}>Create Job </Link>
             </Menu.Item>
             <Menu.Item key="3" icon={<ProfileOutlined />}>
             Job Applications 
@@ -48,28 +88,23 @@ class AdminProfile extends Component {
                 <p>Resume Maker Cum PortFolio </p>
           </Header>
           <Content      
-            style={{margin: '24px 16px 0', overflow: 'initial' ,minHeight:'78vh'}}
+            style={{margin: '24px', overflow: 'initial' ,minHeight:'78vh'}}
           >
-            <Breadcrumb style={{ margin: '16px 0',fontSize:'15px'}}>
+            <Breadcrumb style={{ margin: '8px 0px',fontSize:'15px'}}>
               <Breadcrumb.Item><Text style={{
                 color:'black',
                 fontSize:'25px'
-              }}>User / Profile</Text></Breadcrumb.Item>
+              }}>{this.state.firstname} / Profile</Text></Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{ paddingTop: 36, paddingLeft:"200px"}}>
-                <p>Hello Harsh</p>
-              {/* {this.state.firstname+' '+this.state.lastname} */}
+            <div style={{ paddingLeft:"200px"}}>
               <Col span={18}
               style={{
                 overflowY:'scroll' 
               }}>
-              {/* <Switch>
-               <Route exact path ='/admin-profile/manage' component={ManageOpenening}></Route>
-               <Route exact path='/profile/:userid/edu/:id' component={Edu}></Route>
-               <Route exact path='/profile/:userid/work/:id' component={Work}></Route>
-               <Route exact path='/profile/:userid/skill/:id' component={Skill}></Route>
-               <Route exact path='/profile/:userid/achievement/:id' component={Achievement}></Route>                         
-            </Switch> */}
+              <Switch>
+               <Route exact path ='/admin-profile/:empid/manage/:empid' component={ManageOpenening}></Route>
+               <Route exact path ='/admin-profile/:empid/create-job/:empid' component={CreateJob}></Route>
+            </Switch>
             </Col>
             </div>
           </Content>
